971ea42933ca33738d9181d4d5c91cf36d2103ac
szaaamerik <szaamerik@outlook.com>
2023-06-08 13:43:59 +0200

    committing json implemntation i guesss?
    - everything works apart from sorting

--- Mod-Manager-V2/MainWindow.xaml.cs
+++ Mod-Manager-V2/MainWindow.xaml.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Windows;
-using IniParser.Model;
-using IniParser;
 using MahApps.Metro.Controls;
 using Mod_Manager_V2.Resources;
 using System.Windows.Input;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
-using System.Diagnostics;
 
 namespace Mod_Manager_V2
 {
     public partial class MainWindow : MetroWindow
     {
-        public List<ModPage> ModPages { get; set; }
+        private ModPageParser modPageParser;
+
+        public List<ModPage> modPages { get; set; }
 
         public MainWindow()
         {
@@ -27,66 +26,14 @@ namespace Mod_Manager_V2
             if (!bool.Parse(Settings["Settings"]["Usermode"])) { CheckForPath.CheckIfFolderExists(); } else { CheckForAdmin.FirstLaunch(); }*/
             #endregion
             SettingsFile.CheckForDiscordRPC();
-            ModPages = new List<ModPage>();
-
-            ModPages.Add(new ModPage
-            {
-                Id = 1,
-                Name = "Mod 1",
-                Version = "6.0",
-                Creator = "afatcock#1234",
-                Description = "This is Mod 1",
-                ImageLink = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2F2019%2F11%2Fcat-10-e1573844975155.jpg&f=1&nofb=1&ipt=38c2a3a9ca86eb6a231b3839c7e1668636dd79392cde7193f0b5fa34d487ff08&ipo=images",
-                FileLink = "https://example.com/mod1.zip",
-                UploadDate = "69/69/69",
-                FilePaths = new string[] { "path/to/file1", "path/to/file2" }
-            });
-
-            ModPages.Add(new ModPage
-            {
-                Id = 2,
-                Name = "Mod 2",
-                Version = "7.3",
-                Creator = "nuigggasfatcock#5678",
-                Description = "This is Mod 2asdasdasdasda",
-                ImageLink = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fthewowstyle.com%2Fwp-content%2Fuploads%2F2015%2F04%2FGray-Cat-MorgueFile-Nov16th-2013.jpg&f=1&nofb=1&ipt=6aedfc7e87fa4d6c4fccabb129d62463ec1ef4c3040d62c3993c20d78992e578&ipo=images",
-                FileLink = "https://example.com/mod2.zip",
-                UploadDate = "69/69/69",
-                FilePaths = new string[] { "path/to/file3", "path/to/file4", "path/to/file5" }
-            });
-
-            ModPages.Add(new ModPage
-            {
-                Id = 3,
-                Name = "Mod 3",
-                Version = "1.2",
-                Creator = "zfatcock#5678",
-                Description = "This is Mod 3",
-                ImageLink = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fthewowstyle.com%2Fwp-content%2Fuploads%2F2015%2F04%2FGray-Cat-MorgueFile-Nov16th-2013.jpg&f=1&nofb=1&ipt=6aedfc7e87fa4d6c4fccabb129d62463ec1ef4c3040d62c3993c20d78992e578&ipo=images",
-                FileLink = "https://example.com/mod2.zip",
-                UploadDate = "69/69/69",
-                FilePaths = new string[] { "path/to/file3", "path/to/file4", "path/to/file5" }
-            });
-
-            ModPages.Add(new ModPage
-            {
-                Id = 4,
-                Name = "Mod 4",
-                Version = "8.9",
-                Creator = "bfatcock#5678",
-                Description = "This is Mod 4",
-                ImageLink = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fthewowstyle.com%2Fwp-content%2Fuploads%2F2015%2F04%2FGray-Cat-MorgueFile-Nov16th-2013.jpg&f=1&nofb=1&ipt=6aedfc7e87fa4d6c4fccabb129d62463ec1ef4c3040d62c3993c20d78992e578&ipo=images",
-                FileLink = "https://example.com/mod2.zip",
-                UploadDate = "69/69/69",
-                FilePaths = new string[] { "path/to/file3", "path/to/file4", "path/to/file5" }
-            });
-
-            DataContext = this;
+            modPages = new List<ModPage>();
+            modPageParser = new ModPageParser();
         }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            List<ModPage> modPages = await modPageParser.ParseModPagesFromGitHub();
+            modItemsControl.ItemsSource = modPages;
         }
 
         private void Settings_Button(object sender, RoutedEventArgs e)
@@ -110,40 +57,42 @@ namespace Mod_Manager_V2
 
         private void CategoryButton_Click(object sender, RoutedEventArgs e)
         {
+            // shit doesnt work for some reason?
+
             Button categoryButton = (Button)sender;
-            string category = categoryButton.Content.ToString();
+            string? category = categoryButton.Content.ToString();
             List<ModPage> sortedModPages;
 
             switch (category)
             {
-                case "Default":
-                    sortedModPages = ModPages; // no sorting needed, display all mod pages
+                case "All":
+                    sortedModPages = modPages; // no sorting needed, display all mod pages
                     break;
                 case "Name":
-                    sortedModPages = ModPages.OrderBy(m => m.Name).ToList(); // sort by name
+                    sortedModPages = modPages.OrderBy(m => m.Name).ToList(); // sort by name
                     break;
                 case "Version":
-                    sortedModPages = ModPages.OrderBy(m => m.Version).ToList(); // sort by version
+                    sortedModPages = modPages.OrderBy(m => m.Version).ToList(); // sort by version
                     break;
                 case "Creator":
-                    sortedModPages = ModPages.OrderBy(m => m.Creator).ToList(); // sort by creator
+                    sortedModPages = modPages.OrderBy(m => m.Creator).ToList(); // sort by creator
                     break;
                 case "Upload Date":
-                    sortedModPages = ModPages.OrderBy(m => m.UploadDate).ToList(); // sort by upload date
+                    sortedModPages = modPages.OrderBy(m => DateTime.Parse(m.UploadDate)).ToList(); // sort by upload date
                     break;
                 default:
-                    sortedModPages = ModPages; // needs to be here or the updating fucks itself and default sorting is killed
+                    sortedModPages = modPages; // needs to be here or the updating fucks itself and default sorting is killed
                     break;
             }
 
-            // update the itemssource of the itemscontrol to display the sorted mod pages
+            // update the items of the items to display the sorted mod pages
             modItemsControl.ItemsSource = sortedModPages;
         }
 
         private void DownloadButton_Click(object sender, RoutedEventArgs e)
         {
             Button downloadButton = (Button)sender;
-            ModPage modPage = downloadButton.DataContext as ModPage;
+            ModPage? modPage = downloadButton.DataContext as ModPage;
 
             // i will perform the download logic here using the "modpage.filelink"
 
--- Mod-Manager-V2/Resources/ModPage.cs
+++ Mod-Manager-V2/Resources/ModPage.cs
@@ -1,4 +1,9 @@
-﻿namespace Mod_Manager_V2.Resources
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Mod_Manager_V2.Resources
 {
     public class ModPage
     {
@@ -10,7 +15,36 @@
         public string ImageLink { get; set; }
         public string FileLink { get; set; }
         public string UploadDate { get; set; }
-        public string Category { get; set; }
-        public string[] FilePaths { get; set; }
+        public List<string> FilePaths { get; set; }
+    }
+
+    public class ModPageParser
+    {
+        private const string rawUrl = "https://cdn.discordapp.com/attachments/555877092731125820/1116324036788629584/test.json";
+
+        public async Task<List<ModPage>> ParseModPagesFromGitHub()
+        {
+            string json = await DownloadJsonFromUrl(rawUrl);
+            JObject jsonObject = JObject.Parse(json);
+            JArray modsArray = (JArray)jsonObject["mods"];
+            List<ModPage> modPages = modsArray.ToObject<List<ModPage>>();
+            return modPages;
+        }
+
+        private async Task<string> DownloadJsonFromUrl(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+        }
     }
 }
